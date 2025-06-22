68e9aa58fa7139c7bba780d8146f127394ca4ba6
BharatCodes <bharatydv712@email.com>
2025-06-22 20:34:46 +0530

    Frontend Done Finally

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:1000/api',  // change this
+  baseURL: 'https://courselisterbackend.onrender.com/api',  // change this
   headers: {
     'Content-Type': 'application/json',
   },
